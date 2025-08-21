48a1c8fdb2cdeb30b7e91775dc4771b48220d190
ailicic <codemypath@gmail.com>
2025-08-21 13:08:26 +0200

    Adding AKS cluster]

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-	backend "azurerm" {
-        subscription_id = var.subscription_id
-		resource_group_name = "rg-Aleksandar-Ilicic"
-		storage_account_name = "rgailicictfstate"
-		container_name = "tfstate"
-		key = "key.tfstate"
-	}
-}
